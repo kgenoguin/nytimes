d476b9d9115971b1568e69c7968d10ba46bf8cea
Kier Julian Genoguin <k.genoguin@klab.com>
2016-07-14 12:19:55 +0800

    Made some corrections and placed link for each news

--- news/views.py
+++ news/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from django.template.defaulttags import register
 import requests as req
+import json
 
 
 def news(request):
     base_url = 'http://www.nytimes.com'
     r = req.get(base_url)
     soup = BeautifulSoup(r.text)
-    news_title = []
+    news = []
 
 
-    for story_heading in soup.find_all(class_="story-heading"):
+    for story_heading in soup.find_all(class_= "story-heading"):
+        news_title = ""
+
+
         if story_heading.a:
-            news_title.append(story_heading.a.text.replace("\n", " ").strip())
+            news_title = story_heading.a.text.replace("\n", " ").strip()
         else:
-            news_title.append(story_heading.contents[0].strip())
+            news_title = story_heading.contents[0].strip()
 
-    news_title.sort()
-    return render(request, 'news.html', {'news' : news_title})
+        news.append({ 'title': news_title , 'link': story_heading })
+
+
+    return render(request, 'news.html', {'news' : news})
